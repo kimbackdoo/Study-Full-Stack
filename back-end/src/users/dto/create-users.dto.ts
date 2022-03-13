1879c0b54672cc70d4cce41350537478f20f0f5e
kimbackdoo <kdk7121743@naver.com>
2022-03-13 12:28:31 +0900

    fix: OmitType에서 PickType으로 수정

--- back-end/src/users/dto/create-users.dto.ts
+++ back-end/src/users/dto/create-users.dto.ts
@@ -1,5 +1,6 @@
-import { OmitType } from '@nestjs/swagger'
+import { PickType } from '@nestjs/swagger'
 import { User } from '../users.schema'
 
-// ?? 파일명 users.request.dto 대신 create-users.dto는 어떤지 고민
-export class CreateUserDto extends OmitType(User, ['virtualUser'] as const) {}
+// OmitType은 해당 요소만 제거해서 가져오기 때문에 prototype도 추가적으로 배열에 넣어줘야함
+// 따라서, Picktype을 사용하여 해당 요소들만 가져오게끔 하면됨
+export class CreateUserDto extends PickType(User, ['email', 'name', 'password'] as const) {}
