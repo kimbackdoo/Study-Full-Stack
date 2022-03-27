2f5cebbe90a06047fdb4f53dadce13ab4ea3f898
kimbackdoo <kdk7121743@naver.com>
2022-03-27 16:43:32 +0900

    feat: 추상화 단계를 높이기 위해 custom decorator 추가

--- back-end/src/users/users.controller.ts
+++ back-end/src/users/users.controller.ts
@@ -1,8 +1,11 @@
-import { Body, Controller, Get, Post } from '@nestjs/common'
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common'
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
 import { UsersService } from './users.service'
 import { VirtualUserDto } from './dto/users.dto'
 import { CreateUserDto } from './dto/create-users.dto'
+import { JwtAuthGuard } from './../auth/jwt/jwt.guard'
+import { CurrentUser } from 'src/common/decorators/user.decorator'
+import { User } from 'src/users/users.schema'
 
 @ApiTags('users')
 @Controller('users')
@@ -10,9 +13,10 @@ export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
     @ApiOperation({ summary: '사용자 조회' })
+    @UseGuards(JwtAuthGuard)
     @Get()
-    getCurrentUser() {
-        return 'current user'
+    getCurrentUser(@CurrentUser() user: User) {
+        return user.virtualUser
     }
 
     @ApiOperation({ summary: '회원가입' })
@@ -21,18 +25,6 @@ export class UsersController {
     @Post()
     async createUser(@Body() createUsersDto: CreateUserDto) {
         return await this.usersService.createUser(createUsersDto)
-    }
-
-    @ApiOperation({ summary: '로그인' })
-    @Post('login')
-    logIn() {
-        return 'log in'
-    }
-
-    @ApiOperation({ summary: '로그아웃' })
-    @Post('logout')
-    logOut() {
-        return 'log out'
     }
 
     @ApiOperation({ summary: '이미지 업로드' })
