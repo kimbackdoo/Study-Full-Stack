0e78c4fe67f91178383e950d685451c31ea28487
kimbackdoo <kdk7121743@naver.com>
2022-04-14 01:28:12 +0900

    feat: 회원가입 페이지 생성 및 코드 작성중

---
+++ front-end/pages/sign-up.tsx
@@ -0,0 +1,11 @@
+function SignUp() {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <h3 className="font-bold text-zinc-600">회원가입</h3>
+            <div>이메일</div>
+            <div>비밀번호</div>
+        </div>
+    )
+}
+
+export default SignUp
