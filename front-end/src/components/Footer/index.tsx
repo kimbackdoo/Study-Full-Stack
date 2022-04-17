839cb389bae17b35fdc90b9c4ce6ca43defdcc45
kimbackdoo <kdk7121743@naver.com>
2022-04-17 15:21:07 +0900

    fix: Footer 코드 수정

--- front-end/src/components/Footer/index.tsx
+++ front-end/src/components/Footer/index.tsx
@@ -1,5 +1,29 @@
+import Icon from '@components/Icon'
+
 function Footer() {
-    return <footer>this is footer area</footer>
+    return (
+        <footer className="flex items-center justify-between h-20">
+            <span className="font-bold text-sm">Copyright ⓒ 김재훈 All rights reserved.</span>
+            <div className="flex items-center h-full">
+                <a
+                    className="w-7 h-7 hover:animate-bounce"
+                    target="_blank"
+                    href="https://www.notion.so/KimJeaHun-d6ab9bf0c9ae490aafe05e9e40099d06"
+                    rel="noopener noreferrer"
+                >
+                    <Icon icon="notion" />
+                </a>
+                <a
+                    className="ml-2 w-7 h-7 hover:animate-bounce"
+                    target="_blank"
+                    href="https://github.com/kimbackdoo"
+                    rel="noopener noreferrer"
+                >
+                    <Icon icon="github" />
+                </a>
+            </div>
+        </footer>
+    )
 }
 
 export default Footer
