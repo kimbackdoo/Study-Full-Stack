6c2da30afdcdb9ea9cabcf991ac7442ed3ecec10
kimbackdoo <kdk7121743@naver.com>
2022-04-17 17:22:58 +0900

    fix: Icon 타입에 password-hide, password-show 아이콘 추가

--- front-end/src/components/Icon/constants.ts
+++ front-end/src/components/Icon/constants.ts
@@ -1,6 +1,8 @@
-export type IconName = 'github' | 'notion'
+export type IconName = 'github' | 'notion' | 'password-show' | 'password-hide'
 
 export const iconPath: Record<IconName, string> = {
     github: '/icon/github-logo.svg',
     notion: '/icon/notion-logo.svg',
+    'password-show': '/icon/password-show.svg',
+    'password-hide': '/icon/password-hide.svg',
 }
