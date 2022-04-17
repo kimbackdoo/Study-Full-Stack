dd9c7c3d9fa9932cc272750096bf37f0b243fc90
kimbackdoo <kdk7121743@naver.com>
2022-04-17 17:24:49 +0900

    feat: Input 컴포넌트 설계 및 작성

---
+++ front-end/src/components/Input/constants.ts
@@ -0,0 +1,5 @@
+export const DEFAULT =
+    'px-3 py-2 w-full rounded-md shadow border-2 border-gray-300 appearance-none leading-tight transition-all'
+
+export const PSEUDOS =
+    'focus:outline-none focus:shadow-outline focus:border-4 focus:border-blue-300'
--- front-end/src/components/Input/index.tsx
+++ front-end/src/components/Input/index.tsx
@@ -1,9 +1,12 @@
+import { DEFAULT, PSEUDOS } from '@components/Input/constants'
 import { forwardRef, HTMLAttributes } from 'react'
 
-interface InputProps extends HTMLAttributes<HTMLInputElement> {}
+interface InputProps extends HTMLAttributes<HTMLInputElement> {
+    type?: string
+}
 
-const Input = forwardRef<HTMLInputElement>((_, ref) => {
-    return <input />
+const Input = forwardRef<HTMLInputElement, InputProps>(({ type = 'text', ...rest }, ref) => {
+    return <input className={`${DEFAULT} ${PSEUDOS}`} ref={ref} type={type} {...rest} />
 })
 
 export default Input
