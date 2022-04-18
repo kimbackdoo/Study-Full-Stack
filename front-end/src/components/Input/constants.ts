2bab01fff6a91a2be648cf8be1728e35338fa0c6
kimbackdoo <kdk7121743@naver.com>
2022-04-19 00:54:49 +0900

    fix: Input 컴포넌트 유연하게 사용할 수 있게 수정중

--- front-end/src/components/Input/constants.ts
+++ front-end/src/components/Input/constants.ts
@@ -1,5 +1,4 @@
 export const DEFAULT =
-    'px-3 py-2 w-full rounded-md shadow border-2 border-gray-300 appearance-none leading-tight transition-all'
+    'flex items-center px-3 py-2 w-full rounded-md shadow border-2 border-gray-300 appearance-none leading-tight transition-all'
 
-export const PSEUDOS =
-    'focus:outline-none focus:shadow-outline focus:border-4 focus:border-blue-300'
+export const PSEUDOS = 'outline-none shadow-outline border-4 border-blue-300'
--- front-end/src/components/Input/index.tsx
+++ front-end/src/components/Input/index.tsx
@@ -2,11 +2,17 @@ import { DEFAULT, PSEUDOS } from '@components/Input/constants'
 import { forwardRef, HTMLAttributes } from 'react'
 
 interface InputProps extends HTMLAttributes<HTMLInputElement> {
-    type?: string
+    type?: 'text' | 'password' | 'email'
 }
 
+// TODO Input 컴포넌트 유연하게 사용할 수 있도록 설계하고 코드 쓸 것
 const Input = forwardRef<HTMLInputElement, InputProps>(({ type = 'text', ...rest }, ref) => {
-    return <input className={`${DEFAULT} ${PSEUDOS}`} ref={ref} type={type} {...rest} />
+    return (
+        <div className={`${DEFAULT} ${PSEUDOS}`}>
+            <input className="w-full outline-none" ref={ref} type={type} {...rest} />
+        </div>
+    )
 })
 
+Input.displayName = 'Input'
 export default Input
