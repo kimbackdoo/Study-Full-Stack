939bd0e2b236548da29cce97acee787940465bc0
kimbackdoo <kdk7121743@naver.com>
2022-04-18 09:56:57 +0900

    fix: Label 컴포넌트에 required 속성 추가

--- front-end/src/components/Label/index.tsx
+++ front-end/src/components/Label/index.tsx
@@ -2,13 +2,15 @@ import { HTMLAttributes, ReactNode } from 'react'
 
 interface LabelProps extends HTMLAttributes<HTMLLabelElement> {
     htmlFor?: string
+    required?: boolean
     children?: ReactNode
 }
 
-function Label({ children, ...rest }: LabelProps) {
+function Label({ required, children, ...rest }: LabelProps) {
     return (
         <label className="block text-gray-700 text-sm font-bold mb-2" {...rest}>
-            {children}
+            {children}&nbsp;
+            {required && <span className="text-rose-600">*</span>}
         </label>
     )
 }
