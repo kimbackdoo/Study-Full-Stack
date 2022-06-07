5dceddb69a70f2ea3126819be5657dc375e49cb9
kimbackdoo <kdk7121743@naver.com>
2022-06-08 00:00:28 +0900

    feat: logo 아이콘 추가

--- front-end/src/components/Icon/constants.ts
+++ front-end/src/components/Icon/constants.ts
@@ -1,6 +1,7 @@
-export type IconName = 'github' | 'notion' | 'password-show' | 'password-hide'
+export type IconName = 'logo' | 'github' | 'notion' | 'password-show' | 'password-hide'
 
 export const iconPath: Record<IconName, string> = {
+    logo: '/icon/logo.svg',
     github: '/icon/github-logo.svg',
     notion: '/icon/notion-logo.svg',
     'password-show': '/icon/password-show.svg',
